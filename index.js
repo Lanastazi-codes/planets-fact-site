54e09290d370af86ea9320084be16284cc7c172e
Anastasia <114239649+Lanastazi-codes@users.noreply.github.com>
2023-01-17 16:33:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const nav = document.getElementById("nav");
+const burger = document.getElementById("burger");
+const heading = document.querySelector("h2");
+
+const overviewContent = document.querySelector(".overview-content");
+const planetMenu = document.querySelector(".planet-menu");
+
+burger.addEventListener("click", () => {
+    nav.classList.contains("active")
+        ? nav.classList.remove("active")
+        : nav.classList.add("active");
+    burger.classList.contains("active")
+        ? burger.classList.remove("active")
+        : burger.classList.add("active");
+    burger.classList.contains("active")
+        ? overviewContent.classList.add("hovered")
+        : overviewContent.classList.remove("hovered");
+    burger.classList.contains("active")
+        ? planetMenu.classList.add("hovered")
+        : planetMenu.classList.remove("hovered");
+});
+
+// ? Planet menu
+const overviewBtn = document.querySelector(".planet-menu__overview");
+
+const structureBtn = document.querySelector(".planet-menu__structure");
+const structureContent = document.querySelector(".structure-content");
+console.log(structureBtn);
+
+const surfaceBtn = document.querySelector(".planet-menu__surface");
+const surfaceContent = document.querySelector(".surface-content");
+
+const dash = document.querySelectorAll(".planet-menu__item-dash");
+console.log(dash[0]);
+
+structureBtn.addEventListener("click", () => {
+    structureContent.classList.add("active");
+    overviewContent.classList.remove("active");
+    surfaceContent.classList.remove("active");
+    overviewContent.classList.add("d-non");
+    surfaceContent.classList.add("d-non");
+    dash[1].classList.add("active");
+    dash[0].classList.remove("active");
+    dash[2].classList.remove("active");
+    dash[0].classList.add("hidden");
+    dash[2].classList.add("hidden");
+});
+overviewBtn.addEventListener("click", () => {
+    overviewContent.classList.add("active");
+    structureContent.classList.remove("active");
+    surfaceContent.classList.remove("active");
+    structureContent.classList.add("d-non");
+    surfaceContent.classList.add("d-non");
+    dash[0].classList.add("active");
+    dash[1].classList.remove("active");
+    dash[2].classList.remove("active");
+    dash[1].classList.add("hidden");
+    dash[2].classList.add("hidden");
+});
+surfaceBtn.addEventListener("click", () => {
+    surfaceContent.classList.add("active");
+    structureContent.classList.remove("active");
+    overviewContent.classList.remove("active");
+    structureContent.classList.add("d-non");
+    overviewContent.classList.add("d-non");
+    dash[2].classList.add("active");
+    dash[1].classList.remove("active");
+    dash[0].classList.remove("active");
+    dash[1].classList.add("hidden");
+    dash[0].classList.add("hidden");
+});
